c6cab5a55a1ea46b8e78fb8ec715dbec24e155ec
JuanezAC <juanestebanalzatecuartas@gmail.com>
2026-09-08 18:51:44 -0500

    errores render

---
+++ src/main/java/com/proyect/final_proyect_spa4/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.proyect.final_proyect_spa4.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                    "https://juanezac.github.io",
+                    "http://localhost:4200"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
