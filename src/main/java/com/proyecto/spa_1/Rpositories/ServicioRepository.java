cd000b06c202fc9b2f23609127985cfa6af7ab14
JuanezAC <juanestebanalzatecuartas@gmail.com>
2026-09-08 18:55:13 -0500

    fix: eliminar paquete spa_1 duplicado

--- src/main/java/com/proyecto/spa_1/Rpositories/ServicioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.spa_1.Rpositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.proyecto.spa_1.Entities.Servicio;
-
-public interface ServicioRepository extends JpaRepository<Servicio, Long>{
-
-}
